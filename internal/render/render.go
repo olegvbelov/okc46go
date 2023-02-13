edf9df5b7c8e3dee94eeeb4024cca6ae3933d41a
Oleg Belov <olegvbelov@protonmail.com>
2023-02-13 22:43:59 +0300

    changed phone number (#5)
    Co-authored-by: obelov <obelov@accesssoftek.com>

--- internal/render/render.go
+++ internal/render/render.go
@@ -30,6 +30,7 @@ func AddDefaultData(td *models.TemplateData, r *http.Request) *models.TemplateDa
 	td.Services = models.Services
 	td.Categories = initCategories()
 	td.Phone = "+7 (471-2)36-03-08"
+	td.Shortphone = "+74712360308"
 	return td
 }
 
