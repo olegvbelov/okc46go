5f8444bad7a98edbaf1bf21d6dd44806f3598e32
obelov <obelov@accesssoftek.com>
2023-05-06 16:52:18 +0300

    added service detail template

--- internal/models/models.go
+++ internal/models/models.go
@@ -3,22 +3,31 @@ package models
 import "html/template"
 
 type QuestionAnswer struct {
+	Id       string
 	Question string
 	Answer   string
 }
 
+type PlanDetail struct {
+	Description template.HTML
+	Cost        string
+}
+
 type OkcService struct {
-	Id               string
-	Title            string
-	ShortDescription template.HTML
-	Description      template.HTML
-	Link             string
-	IconUrl          string
-	Photo390x260     string
-	Webp390x260      string
-	OnMain           bool
-	DetailUrl        string
-	QuestionAnswers  []QuestionAnswer
+	Id                string
+	Title             string
+	ShortDescription  template.HTML
+	Description       template.HTML
+	Link              string
+	IconUrl           string
+	Photo390x260      string
+	Webp390x260       string
+	OnMain            bool
+	DetailUrl         string
+	QuestionAnswers   []QuestionAnswer
+	DetailImageUrl    []string
+	DetailDescription []string
+	DetailPlan        []PlanDetail
 }
 
 var Services = []OkcService{
@@ -27,23 +36,38 @@ var Services = []OkcService{
 		Title:            "Межевание участка",
 		ShortDescription: "Проведем для Вас межевание земельного участкаи и подготовим пакет документов для постановки на учет",
 		Description:      "Подготовка межевого плана<br/>Постановка участка на кадастровый учет<br/> Уточнение границ земельного участка",
-		IconUrl:          "/static/images/samples/390x260/image_01.jpg",
 		Photo390x260:     "/static/images/okc46/services/390x260/mega.jpg",
 		Webp390x260:      "/static/images/okc46/services/390x260/mega.webp",
 		OnMain:           true,
-		DetailUrl:        "/servicedetails/%s",
-		Link:             "/services",
+		DetailUrl:        "/details/1",
+		QuestionAnswers: []QuestionAnswer{
+			{"accordion-owner", "Возможно ли оформление меживание не собственником", "Да, при наличии нотариально заверенной доверенности."},
+			{"accordion-agreement", "Кто проводит согласование кадастрового плана с соседями?", "Согласование проводит собственник участка."},
+			{"accordion-passport", "Где можно получить кадастровый паспорт", "Кадастровый паспорт получают в БТИ."},
+		},
+		Link:           "/services",
+		DetailImageUrl: []string{"mega.jpg", "mega1.jpg"},
+		DetailDescription: []string{"Межевание земельного участка - установка на топографической карте местности координат земельного участка, определение и согласование его границ с целью постановки земельного участка на кадастровый учет.",
+			"Межевание позволит решить возможные конфликты с соседями, получить право собственности и совершать юридические операции с земельным участком: дарить, менять, продавать."},
+		DetailPlan: []PlanDetail{{"Подготовить документы на замельный участок: <br/><ul><li class=\"template-arrow-right-2\">заявление, составленное в свободной форме;</li><li class=\"template-arrow-right-2\">паспорт владельца;</li><li class=\"template-arrow-right-2\">правоустанавливающие документы на земельный участок;</li><li class=\"template-arrow-right-2\">правоудостоверяющие документы на земельный участок;</li><li class=\"template-arrow-right-2\">кадастровая выписка;</li><li class=\"template-arrow-right-2\">кадастровый паспорт;</li></ul>",
+			""},
+			{"Заключить договор с кадастровым инженером/землеустроительной организацией", "6000 рублей"},
+			{"Проведение кадастровым инженером замеров и подготовка кадастрового плана", ""},
+			{"Ознакомление с составленным кадастровым планом", ""},
+			{"Согласование кадастрового плана с владельцами смежных участков", ""},
+			{"Отправка заявления и кадастрового плана в орган регистрации прав", ""},
+			{"Получение выписки из ЕГРН", ""},
+		},
 	},
 	{
 		Id:               "2",
 		Title:            "Выдел пая",
 		ShortDescription: "Выдел пая из земель сельхозназначения",
 		Description:      "Межевание земельного участка<br/>Подготовка кадастрового плана<br/>Информационное сопровождение",
-		IconUrl:          "images/icon/3.png",
 		Photo390x260:     "/static/images/okc46/services/390x260/pie.jpg",
 		Webp390x260:      "/static/images/okc46/services/390x260/mega.webp",
 		OnMain:           false,
-		DetailUrl:        "",
+		DetailUrl:        "/services",
 		Link:             "/services",
 	},
 	{
@@ -51,11 +75,10 @@ var Services = []OkcService{
 		Title:            "Топографическая съемка",
 		ShortDescription: "Наши специалисты выполнят топографическую съемку и профессиональную обработу результатов",
 		Description:      "Топографическая съемка земельного участка<br/>Топографическая съемка линейных сооружений<br/>Камеральные исследования<br/>",
-		IconUrl:          "",
 		Photo390x260:     "/static/images/okc46/services/390x260/topo.jpg",
 		Webp390x260:      "/static/images/okc46/services/390x260/topo.webp",
 		OnMain:           true,
-		DetailUrl:        "",
+		DetailUrl:        "/services",
 		Link:             "/services",
 	},
 	{
@@ -63,11 +86,10 @@ var Services = []OkcService{
 		Title:            "Оценка недвижимости",
 		ShortDescription: "Сертифицированные специалисты проведут оценку квартиры, дома, строения и земельного участка",
 		Description:      "Оценка квартиры, дома для ипотеки<br/>Оценка объекта недвижимости для суда<br/>Оценка недвижимости для наследства<br/>",
-		IconUrl:          "",
 		Photo390x260:     "/static/images/okc46/services/390x260/estimation.jpg",
 		Webp390x260:      "/static/images/okc46/services/390x260/estimation.webp",
 		OnMain:           true,
-		DetailUrl:        "",
+		DetailUrl:        "/services",
 		Link:             "/services",
 	},
 	{
@@ -75,11 +97,10 @@ var Services = []OkcService{
 		Title:            "Подготовка плана объекта",
 		ShortDescription: "Подготовим для Вас карту объекта землеустройства",
 		Description:      "Получене сведений государственного кадастра<br/>Определение границ на местности<br/>Проведение согласований",
-		IconUrl:          "images/icon/4.png",
 		Photo390x260:     "/static/images/okc46/services/390x260/objectplan.jpg",
 		Webp390x260:      "/static/images/okc46/services/390x260/objectplan.webp",
 		OnMain:           false,
-		DetailUrl:        "",
+		DetailUrl:        "/services",
 		Link:             "/services",
 	},
 	{
@@ -87,11 +108,10 @@ var Services = []OkcService{
 		Title:            "Технический план",
 		ShortDescription: "Подготовим для Вас технический план здания, помещения",
 		Description:      "Подготовка технического плана здания, помещения<br/>Регистрация права на объект недвижимости<br/>Внесение сведений об изменении в ЕГРН",
-		IconUrl:          "images/icon/2.png",
 		Photo390x260:     "/static/images/okc46/services/390x260/techplan.jpg",
 		Webp390x260:      "/static/images/okc46/services/390x260/objectplan.webp",
 		OnMain:           false,
-		DetailUrl:        "",
+		DetailUrl:        "/services",
 		Link:             "/services",
 	},
 	{
@@ -99,11 +119,10 @@ var Services = []OkcService{
 		Title:            "Вынос точек",
 		ShortDescription: "Вынос границ земельного участка в натуру",
 		Description:      "Вынос координат и точек с соблюдением требований СНиП<br/>Установка временных или постоянных межевых знаков<br/>Составление акта",
-		IconUrl:          "images/icon/1.png",
 		Photo390x260:     "/static/images/okc46/services/390x260/vinos.jpg",
 		Webp390x260:      "/static/images/okc46/services/390x260/vinos.webp",
 		OnMain:           false,
-		DetailUrl:        "",
+		DetailUrl:        "/services",
 		Link:             "/services",
 	},
 	{
@@ -111,11 +130,10 @@ var Services = []OkcService{
 		Title:            "Энергоаудит",
 		ShortDescription: "Энергоаудит",
 		Description:      "Испытание зданий, жилых помещений на воздхо и тепло проницаемость<br/>Поиск утечек тепла, замеры, консльтации, отчеты</br>Сертифицированное обордование",
-		IconUrl:          "images/icon/1.png",
 		Photo390x260:     "/static/images/okc46/services/390x260/teplovisor.jpg",
 		Webp390x260:      "/static/images/okc46/services/390x260/teplovisor.webp",
 		OnMain:           false,
-		DetailUrl:        "",
+		DetailUrl:        "/services",
 		Link:             "/services",
 	},
 }
